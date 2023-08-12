55a89e83295a68701cdbaf9c3b4448c24ce99951
Vural Kayra Cetintas <73638860+vuralkayracetintas@users.noreply.github.com>
2023-08-12 10:04:39 +0300

    ad_helper edited

--- lib/package/ad_helper.dart
+++ lib/package/ad_helper.dart
@@ -3,9 +3,9 @@ import 'dart:io';
 class AdHelper {
   static String get bannerAdUnitId {
     if (Platform.isAndroid) {
-      return 'ca-app-pub-2445169075279341/3779906560';
+      return '';
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-2445169075279341/9764470184';
+      return '';
       //'ca-app-pub-2445169075279341/9764470184';
     } else {
       throw UnsupportedError('Unsupported platform');
@@ -14,9 +14,9 @@ class AdHelper {
 
   static String get interstitialAdUnitId {
     if (Platform.isAndroid) {
-      return "ca-app-pub-2445169075279341/5593156465";
+      return "";
     } else if (Platform.isIOS) {
-      return 'ca-app-pub-2445169075279341/9298616077'; // 'ca-app-pub-2445169075279341/9298616077';
+      return '';
     } else {
       throw UnsupportedError('Unsupported platform');
     }
